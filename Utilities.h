a4429c20088b3ec4bcad5d57f654e233df2361a0
codernineteen <wjsdPcks34@gmail.com>
2024-02-04 18:58:52 +0900

    Common includes

--- Utilities.h
+++ Utilities.h
@@ -1,3 +1,14 @@
 #pragma once
 
+// vulkan
+#include <vulkan/vulkan.h>
+
+// glfw
+#include <GLFW/glfw3.h>
+
+// std
 #include <cstdint>
+#include <iostream>
+#include <vector>
+#include <set>
+#include <memory> // for smart pointers
