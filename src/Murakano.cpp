9d4f409694313ce03bd21917be96aec0a9b5e3d0
codernineteen <wjsdPcks34@gmail.com>
2024-02-13 14:42:11 +0900

    Update usage

--- src/Murakano.cpp
+++ src/Murakano.cpp
@@ -4,4 +4,12 @@ int main()
 {
 	MKRenderer renderer;
 	renderer.Render();
+
+#ifdef _MSC_VER
+#    ifdef NDEBUG
+#        pragma comment(linker, "/SUBSYSTEM:WINDOWS /ENTRY:mainCRTStartup")
+#    else
+#        pragma comment(linker, "/SUBSYSTEM:CONSOLE")
+#    endif
+#endif
 }
