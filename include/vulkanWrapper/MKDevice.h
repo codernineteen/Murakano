0dea8d67ec00025d107b9f7e117525be7e80d289
codernineteen <wjsdPcks34@gmail.com>
2024-02-13 14:39:02 +0900

    Device class modified - query swap chain support detail - helper function to choose optimal informations for swapchain creation

--- include/vulkanWrapper/MKDevice.h
+++ include/vulkanWrapper/MKDevice.h
@@ -1,12 +1,20 @@
 #pragma once
 
-// internal
+// std
 #include <map>
 #include <optional>
+
+// internal
 #include "Utilities.h"
 #include "MKInstance.h"
 
 
+// [MKWindow class]
+// - Responsibility :
+//	  - responsible for finding a suitable physical device and creating a logical device.
+// - Dependency :
+//    - MKInstance as reference
+//    - MKSwapchain as member variable
 class MKDevice 
 {
 public:
@@ -29,23 +37,37 @@ public:
 	};
 
 public:
-	MKDevice(const MKInstance& mkInstance);
+	MKDevice(const MKWindow& windowRef, const MKInstance& instanceRef);
 	~MKDevice();
+	VkPhysicalDevice GetPhysicalDevice() const { return _physicalDevice; }
 	VkDevice GetDevice() const { return _logicalDevice; }
+	VkSurfaceKHR GetSurface() const { return _surface; }
 
-private:
-	void PickPhysicalDevice(const MKInstance& mkInstance);
-	int RateDeviceSuitability(VkPhysicalDevice device);
-	QueueFamilyIndices FindQueueFamilies(VkPhysicalDevice device);
 	SwapChainSupportDetails QuerySwapChainSupport(VkPhysicalDevice device);
-	bool isDeviceExtensionSupported(VkPhysicalDevice device);
+	QueueFamilyIndices FindQueueFamilies(VkPhysicalDevice device);
+	
+	// Belows are related to swapchain creation.
+	// I located these functions in Device class for consistency vecause one of them requires window refernce.
+	VkSurfaceFormatKHR ChooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats);
+	VkPresentModeKHR ChooseSwapPresentMode(const std::vector<VkPresentModeKHR>& availablePresentModes);
+	VkExtent2D ChooseSwapExtent(const VkSurfaceCapabilitiesKHR& capabilities);
 
 private:
-	VkPhysicalDevice _physicalDevice = VK_NULL_HANDLE;
-	VkDevice _logicalDevice = VK_NULL_HANDLE;
-	VkSurfaceKHR _surface = VK_NULL_HANDLE;
+	void PickPhysicalDevice();
+	int RateDeviceSuitability(VkPhysicalDevice device);
+	bool IsDeviceExtensionSupported(VkPhysicalDevice device);
+	void CreateWindowSurface();
+
+private:
+	VkPhysicalDevice _physicalDevice = VK_NULL_HANDLE;	// actual physical device
+	VkDevice _logicalDevice = VK_NULL_HANDLE;			// an interface to communicate with physical device
+	VkSurfaceKHR _surface = VK_NULL_HANDLE;				// an interface to communicate with the window system
 	VkQueue _graphicsQueue;
 	VkQueue _presentQueue;
+
+private:
+	const MKInstance& _mkInstanceRef;
+	const MKWindow& _mkWindowRef;
 
 public:
 	const std::vector<const char*> deviceExtensions = {
