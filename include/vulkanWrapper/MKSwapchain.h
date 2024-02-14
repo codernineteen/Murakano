380fa1a1e28034d80b26b4ecf848fac220963f98
codernineteen <wjsdPcks34@gmail.com>
2024-02-14 21:52:18 +0900

    Abstraction for swapchain creation

--- include/vulkanWrapper/MKSwapchain.h
+++ include/vulkanWrapper/MKSwapchain.h
@@ -1,16 +1,35 @@
+#pragma once
+
 // internal
 #include "Utilities.h"
 #include "MKDevice.h"
 
+// [MKSwapchain class]
+// - Responsibility :
+//    - swapchain and image management.
+// - Dependency :
+//    - MkDevice as refernece
 class MKSwapchain
 {
 public:
 	MKSwapchain(MKDevice& deviceWrapper);
 	~MKSwapchain();
 
-private:
-	VkSwapchainKHR _vkSwapchain;
+public:
+	VkFormat GetSwapchainImageFormat() const { return _vkSwapchainImageFormat; }
+	// reusable image view creation
+	VkImageView CreateImageView(VkImage image, VkFormat format, VkImageAspectFlags aspectFlags, uint32_t mipLevels);
 
+private:
+	void CreateSwapchainImageViews();
+
+private:
+	VkSwapchainKHR				_vkSwapchain;
+	std::vector<VkImage>		_vkSwapchainImages;
+	std::vector<VkImageView>	_vkSwapchainImageViews;
+	VkFormat					_vkSwapchainImageFormat;
+	VkExtent2D					_vkSwapchainExtent;
+		
 private:
 	MKDevice& _deviceRef;
 
--- src/vulkanWrapper/MKSwapchain.cpp
+++ src/vulkanWrapper/MKSwapchain.cpp
@@ -48,10 +48,53 @@ MKSwapchain::MKSwapchain(MKDevice& deviceRef)
 
 	if (vkCreateSwapchainKHR(_deviceRef.GetDevice(), &swapchainCreateInfo, nullptr, &_vkSwapchain) != VK_SUCCESS)
 		throw std::runtime_error("Failed to create swapchain");
+
+	// retrieve swapchain images
+	vkGetSwapchainImagesKHR(_deviceRef.GetDevice(), _vkSwapchain, &imageCount, nullptr);
+	_vkSwapchainImages.resize(imageCount);
+	vkGetSwapchainImagesKHR(_deviceRef.GetDevice(), _vkSwapchain, &imageCount, _vkSwapchainImages.data());
+
+	// store swapchain format and extent
+	_vkSwapchainImageFormat = surfaceFormat.format;
+	_vkSwapchainExtent = actualExtent;
+
+	// create image views mapped to teh swapchain images.
+	CreateSwapchainImageViews();
 }
 
 MKSwapchain::~MKSwapchain()
 {
+	for (auto imageView : _vkSwapchainImageViews)
+		vkDestroyImageView(_deviceRef.GetDevice(), imageView, nullptr);
 	vkDestroySwapchainKHR(_deviceRef.GetDevice(), _vkSwapchain, nullptr);
+}
+
+VkImageView MKSwapchain::CreateImageView(VkImage image, VkFormat format, VkImageAspectFlags aspectFlags, uint32_t mipLevels)
+{
+	VkImageViewCreateInfo imageViewCreateInfo{};
+	imageViewCreateInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+	imageViewCreateInfo.image = image;
+	imageViewCreateInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;			// 2D image in most cases.
+	imageViewCreateInfo.subresourceRange.aspectMask = aspectFlags;
+	imageViewCreateInfo.subresourceRange.baseMipLevel = 0;			// first mipmap level accessible to the view
+	imageViewCreateInfo.subresourceRange.levelCount = mipLevels;	// number of mipmap levels accessible to the view
+	imageViewCreateInfo.subresourceRange.baseArrayLayer = 0;		// first array layer accessible to the view
+	imageViewCreateInfo.subresourceRange.layerCount = 1;
+
+	VkImageView imageView;
+	if (vkCreateImageView(_deviceRef.GetDevice(), &imageViewCreateInfo, nullptr, &imageView) != VK_SUCCESS)
+		throw std::runtime_error("Failed to create image views");
+
+	return imageView;
+}
+
+void MKSwapchain::CreateSwapchainImageViews()
+{
+	_vkSwapchainImageViews.resize(_vkSwapchainImages.size());
+
+	for (size_t i = 0; i < _vkSwapchainImages.size(); i++)
+	{
+		_vkSwapchainImageViews[i] = CreateImageView(_vkSwapchainImages[i], _vkSwapchainImageFormat, VK_IMAGE_ASPECT_COLOR_BIT, 1);
+	}
 }
 
