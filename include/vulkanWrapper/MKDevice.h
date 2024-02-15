1caf416adc4013eb129df9a14db0f9bd78d4964e
codernineteen <wjsdPcks34@gmail.com>
2024-02-15 17:35:23 +0900

    Modified dependencies

--- include/vulkanWrapper/MKDevice.h
+++ include/vulkanWrapper/MKDevice.h
@@ -31,46 +31,49 @@ public:
 
 	struct SwapChainSupportDetails
 	{
-		VkSurfaceCapabilitiesKHR capabilities;		// basic surface capabilities (min/max number of images in swap chain, min/max width and height of images)
-		std::vector<VkSurfaceFormatKHR> formats;	// surface formats	(pixel format, color space)
-		std::vector<VkPresentModeKHR> presentModes; // presentation modes (conditions for "swapping" images to the screen)
+		VkSurfaceCapabilitiesKHR			capabilities;	// basic surface capabilities (min/max number of images in swap chain, min/max width and height of images)
+		std::vector<VkSurfaceFormatKHR>		formats;		// surface formats	(pixel format, color space)
+		std::vector<VkPresentModeKHR>		presentModes;	// presentation modes (conditions for "swapping" images to the screen)
 	};
 
 public:
 	MKDevice(const MKWindow& windowRef, const MKInstance& instanceRef);
 	~MKDevice();
-	VkPhysicalDevice GetPhysicalDevice() const { return _physicalDevice; }
-	VkDevice GetDevice() const { return _logicalDevice; }
-	VkSurfaceKHR GetSurface() const { return _surface; }
 
+	// getter
+	VkPhysicalDevice		GetPhysicalDevice() const { return _vkPhysicalDevice; }
+	VkDevice				GetDevice()			const { return _vkLogicalDevice; }
+	VkSurfaceKHR			GetSurface()		const { return _vkSurface; }
+
+	// support checkers
 	SwapChainSupportDetails QuerySwapChainSupport(VkPhysicalDevice device);
-	QueueFamilyIndices FindQueueFamilies(VkPhysicalDevice device);
+	QueueFamilyIndices		FindQueueFamilies(VkPhysicalDevice device);
 	
 	// Belows are related to swapchain creation.
 	// I located these functions in Device class for consistency vecause one of them requires window refernce.
-	VkSurfaceFormatKHR ChooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats);
-	VkPresentModeKHR ChooseSwapPresentMode(const std::vector<VkPresentModeKHR>& availablePresentModes);
-	VkExtent2D ChooseSwapExtent(const VkSurfaceCapabilitiesKHR& capabilities);
+	VkSurfaceFormatKHR		ChooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats);
+	VkPresentModeKHR		ChooseSwapPresentMode(const std::vector<VkPresentModeKHR>& availablePresentModes);
+	VkExtent2D				ChooseSwapExtent(const VkSurfaceCapabilitiesKHR& capabilities);
 
 private:
-	void PickPhysicalDevice();
-	int RateDeviceSuitability(VkPhysicalDevice device);
-	bool IsDeviceExtensionSupported(VkPhysicalDevice device);
-	void CreateWindowSurface();
+	void	PickPhysicalDevice();
+	int		RateDeviceSuitability(VkPhysicalDevice device);
+	bool	IsDeviceExtensionSupported(VkPhysicalDevice device);
+	void	CreateWindowSurface();
 
 private:
-	VkPhysicalDevice _physicalDevice = VK_NULL_HANDLE;	// actual physical device
-	VkDevice _logicalDevice = VK_NULL_HANDLE;			// an interface to communicate with physical device
-	VkSurfaceKHR _surface = VK_NULL_HANDLE;				// an interface to communicate with the window system
-	VkQueue _graphicsQueue;
-	VkQueue _presentQueue;
+	VkPhysicalDevice	_vkPhysicalDevice = VK_NULL_HANDLE;		// actual physical device
+	VkDevice			_vkLogicalDevice = VK_NULL_HANDLE;		// an interface to communicate with physical device
+	VkSurfaceKHR		_vkSurface = VK_NULL_HANDLE;			// an interface to communicate with the window system
+	VkQueue				_vkGraphicsQueue;
+	VkQueue				_vkPresentQueue;
 
 private:
-	const MKInstance& _mkInstanceRef;
-	const MKWindow& _mkWindowRef;
+	const MKInstance&	_mkInstanceRef;
+	const MKWindow&		_mkWindowRef;
 
 public:
 	const std::vector<const char*> deviceExtensions = {
-		VK_KHR_SWAPCHAIN_EXTENSION_NAME // macro from VK_KHR_swapchain extension
+		VK_KHR_SWAPCHAIN_EXTENSION_NAME		// macro from VK_KHR_swapchain extension
 	};
 };
