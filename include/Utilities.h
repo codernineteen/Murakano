5bee6c7a63db696865a8367c351cabc22c02ec59
codernineteen <wjsdPcks34@gmail.com>
2024-02-15 17:36:35 +0900

    added standard library "string"

--- include/Utilities.h
+++ include/Utilities.h
@@ -18,6 +18,7 @@
 #include <memory> // for smart pointers
 #include <algorithm>
 #include <array>
+#include <string>
 
 // helper function
 namespace util 
@@ -26,17 +27,17 @@ namespace util
 	{
         std::ifstream file(filename, std::ios::ate | std::ios::binary);
 
-        if (!file.is_open()) {
+        if (!file.is_open() || file.bad()) {
             throw std::runtime_error("failed to open file!");
         }
 
-        size_t fileSize = (size_t)file.tellg(); // advangtage of ios::ate - we can use read position to determine size of file
+        size_t fileSize = static_cast<size_t>(file.tellg()); // advangtage of ios::ate - we can use read position to determine size of file
         std::vector<char> buffer(fileSize);
 
         file.seekg(0);
         file.read(buffer.data(), fileSize);     // read all bytes at once
-        file.close();
 
+        file.close();
         return buffer;
 	}
 }
