36de85801a3e859ddff14082a8c329e50285c500
codernineteen <wjsdPcks34@gmail.com>
2024-02-24 20:50:16 +0900

    Define UBO struct and its usage.

--- shaders/HLSL/vertex.hlsl
+++ shaders/HLSL/vertex.hlsl
@@ -1,23 +1,32 @@
 struct VSInput
 {
-    [[vk::location(0)]] float3 Position : POSITIONT0;
+    [[vk::location(0)]] float2 Position : POSITION0;
     [[vk::location(1)]] float3 Color : COLOR0;
 };
+
+struct UBO
+{
+    float4x4 mvpMat;
+};
+
+cbuffer ubo : register(b0, space0)
+{
+    UBO ubo;
+}
 
 struct VSOutput
 {
     float4 Pos : SV_POSITION;
-    [[vk::location(0)]]   float3 Color : COLOR0;
+    [[vk::location(0)]] float3 Color : COLOR0;
 };
-
-// hard-coded positions
 
 VSOutput main(VSInput input, uint VertexIndex : SV_VertexID)
 {
-    VSOutput output = (VSOutput)0;
-
-    output.Pos = float4(input.Position, 1.0);
+    VSOutput output = (VSOutput) 0;
     output.Color = input.Color;
-
+    
+    float4 pos = float4(input.Position, 0.0f, 1.0f);
+    output.Pos = mul(pos, ubo.mvpMat);
+   
     return output;
 }
