b42f051b451ce355c24f92d4a80117af68eb0adf
codernineteen <wjsdPcks34@gmail.com>
2024-02-26 20:53:53 +0900

    Removed vertices from pipeline source file

--- Source/Vulkan/Private/MKGraphicsPipeline.cpp
+++ Source/Vulkan/Private/MKGraphicsPipeline.cpp
@@ -1,16 +1,4 @@
 #include "MkGraphicsPipeline.h" 
-
-const std::vector<Vertex> vertices = {
-	{{-0.5f, -0.5f}, {1.0f, 0.0f, 0.0f}},
-	{{0.5f, -0.5f}, {0.0f, 1.0f, 0.0f}},
-	{{0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}},
-	{{-0.5f, 0.5f}, {1.0f, 1.0f, 1.0f}}
-};
-
-const std::vector<uint16> indices = {
-	0, 1, 2, 2, 3, 0
-};
-
 /*
 -----------	PUBLIC ------------
 */
