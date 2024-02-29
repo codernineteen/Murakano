c2ef57f619339d0b0d70d571b6e0fc55f674cd58
codernineteen <wjsdPcks34@gmail.com>
2024-02-29 20:16:48 +0900

    Added a new type name alias for void-returning lambda function

--- Source/Commons/Types.h
+++ Source/Commons/Types.h
@@ -1,8 +1,15 @@
-#include <cstdint>
+#pragma once
 
+#include <cstdint>
+#include <functional>
+
+// integer aliases
 using uint16 = std::uint16_t;
 using uint32 = std::uint32_t;
 using uint64 = std::uint64_t;
 using int16 = std::int16_t;
 using int32 = std::int32_t;
 using int64 = std::int64_t;
+
+// function aliases
+using VoidLambda = std::function<void(VkCommandBuffer)>;
