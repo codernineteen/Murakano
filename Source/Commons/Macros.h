1eaf34080c378fc065e3b2afc89a3164271ae31f
codernineteen <wjsdPcks34@gmail.com>
2024-03-02 16:07:45 +0900

    - removed compile definition - create new macro rules  1. MK_CHECK for checking result of vk prefixed funtion calls  2. MK_LOG for logging system print ouput  3. MK_THROW for throwing exception for single line case

--- Source/Commons/Macros.h
+++ Source/Commons/Macros.h
@@ -1,2 +1,25 @@
-#define GLM_FORCE_RADIANS
-#define STB_IMAGE_IMPLEMENTATION
+#pragma once
+
+#include <fmt/format.h>
+#include <stdexcept>
+#include <vulkan/vk_enum_string_helper.h>
+
+// validation macros ( use do-while(0) to avoid a problem that may happen while macro expansion )
+#define MK_CHECK(x)                                                                        \
+  do 																					   \
+  {																						   \
+	  VkResult err = x;																	   \
+	  if (err)										                                       \
+	  {																					   \
+		  auto msg = fmt::format("[Murakano says] : vulakn error occured - {}\n", string_VkResult(err));  \
+		  throw std::runtime_error(msg);															   \
+	  }																					   \
+  } while(0)      
+
+// log macros
+#define MK_LOG(msg)                                                                        \
+  fmt::print("[Murakano says] : {}\n", msg);
+
+// throw macros
+#define MK_THROW(msg)                                                                      \
+  throw std::runtime_error(fmt::format("[Murakano says] : {}\n", msg));                               											   																				   
