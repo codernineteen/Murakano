b6dfef519fc1ef666eddd178544b3fc94eb04bf2
codernineteen <wjsdPcks34@gmail.com>
2024-03-03 19:09:21 +0900

    Moved types, macros into Public directory

---
+++ Source/Commons/Public/Types.h
@@ -0,0 +1,37 @@
+#pragma once
+
+// third-party
+#include <vulkan/vulkan.h> // vulkan
+#include <fmt/core.h>
+#include <DirectXMath.h>
+
+// std
+#include <cstdint>
+#include <functional>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <set>
+#include <memory>
+#include <algorithm>
+#include <array>
+#include <string>
+#include <stack>
+#include <queue>
+
+// namespace aliases
+namespace dx = DirectX;
+
+// integer aliases
+using uint16 = std::uint16_t;
+using uint32 = std::uint32_t;
+using uint64 = std::uint64_t;
+using int16 = std::int16_t;
+using int32 = std::int32_t;
+using int64 = std::int64_t;
+
+// function aliases
+using VoidLambda = std::function<void(VkCommandBuffer)>;
+
