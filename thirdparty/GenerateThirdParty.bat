e31b41f10f902b46ab52f93c998eabd8f0094411
codernineteen <wjsdPcks34@gmail.com>
2024-03-04 22:54:38 +0900

    cloning third party batch script

---
+++ thirdparty/GenerateThirdParty.bat
@@ -0,0 +1,10 @@
+echo "Cloning third parties from github..."
+
+git clone https://github.com/glfw/glfw/tree/3.3-stable.git
+git clone https://github.com/microsoft/DirectXMath.git
+git clone https://github.com/microsoft/DirectXShaderCompiler.git
+git clone https://github.com/tinyobjloader/tinyobjloader.git
+git clone https://github.com/nothings/stb.git
+git clone https://github.com/fmtlib/fmt.git
+
+ehco "Finished cloning."
