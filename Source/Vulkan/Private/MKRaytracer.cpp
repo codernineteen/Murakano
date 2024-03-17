38dcbbbf373e325d721f7f56972054afd20a4d7c
codernineteen <wjsdPcks34@gmail.com>
2024-03-17 22:47:34 +0900

    add selective vertex, uniform buffer layout and update logic based on shader language

---
+++ Source/Vulkan/Private/MKRaytracer.cpp
@@ -0,0 +1,12 @@
+#include "MKRaytracer.h"
+
+void MKRaytracer::BuildRayTracer()
+{
+// BLAS
+
+}
+
+void MKRaytracer::LoadVkRaytracingExtension(VkDevice device)
+{
+	//vkGetAccelerationStructureBuildSizesKHR = (PFN_vkGetAccelerationStructureBuildSizesKHR)vkGetDeviceProcAddr(device, "vkGetAccelerationStructureBuildSizesKHR");
+}
