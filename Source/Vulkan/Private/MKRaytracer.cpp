08233e78eb51dd76d17a41c7e2db9899b36c4aef
codernineteen <wjsdPcks34@gmail.com>
2024-03-23 22:48:24 +0900

    Implement build bottom level acceleration structure logics

--- Source/Vulkan/Private/MKRaytracer.cpp
+++ Source/Vulkan/Private/MKRaytracer.cpp
@@ -13,7 +13,7 @@ void MKRaytracer::BuildRayTracer()
 // BLAS
 }
 
-MKRaytracer::BLAS MKRaytracer::ObjectToVkGeometryKHR(const OBJModel& model)
+VkBLAS MKRaytracer::ObjectToVkGeometryKHR(const OBJModel& model)
 {
 	// TODO : move vertex and index buffer resources into model class
 	// get raw vetex and index buffer device address
@@ -47,8 +47,57 @@ MKRaytracer::BLAS MKRaytracer::ObjectToVkGeometryKHR(const OBJModel& model)
 	buildRangeInfo.primitiveOffset = 0;
 	buildRangeInfo.transformOffset = 0;
 
-	BLAS blas{ geometry, buildRangeInfo };
+	VkBLAS blas;
+	blas.geometry.emplace_back(geometry);
+	blas.buildRangeInfo.emplace_back(buildRangeInfo);
 	return blas;
+}
+
+void MKRaytracer::CreateBLAS(const OBJModel& model)
+{
+	std::vector<VkBLAS> blases;
+	// TODO : single model to multiple models
+	blases.reserve(1); // reserve space for single model now.
+
+	auto blas = ObjectToVkGeometryKHR(model);
+	blases.emplace_back(blas);
+	
+	BuildBLAS(blases, VK_BUILD_ACCELERATION_STRUCTURE_PREFER_FAST_TRACE_BIT_KHR);
+}
+
+void MKRaytracer::BuildBLAS(const std::vector<VkBLAS>& blaseInputs, VkBuildAccelerationStructureFlagsKHR flags) 
+{
+	uint32 blasCount = static_cast<uint32>(blaseInputs.size());
+
+	std::vector<VkAccelerationStructureKHRInfo> buildAsInfo(blasCount);
+	for (uint32 idx = 0; idx < blasCount; idx++)
+	{
+		// Filling partially the VkAccelerationStructureBuildGeometryInfoKHR for querying the build sizes.
+		// Other information will be filled in the createBlas (see #2)
+		buildAsInfo[idx].buildInfo.type = VK_ACCELERATION_STRUCTURE_TYPE_BOTTOM_LEVEL_KHR;
+		buildAsInfo[idx].buildInfo.mode = VK_BUILD_ACCELERATION_STRUCTURE_MODE_BUILD_KHR;
+		buildAsInfo[idx].buildInfo.flags = blaseInputs[idx].flags | flags;
+		buildAsInfo[idx].buildInfo.geometryCount = static_cast<uint32_t>(blaseInputs[idx].geometry.size());
+		buildAsInfo[idx].buildInfo.pGeometries = blaseInputs[idx].geometry.data();
+		buildAsInfo[idx].rangeInfo = blaseInputs[idx].buildRangeInfo.data();
+
+		// Finding sizes to create acceleration structures and scratch
+		std::vector<uint32_t> maxPrimCount(blaseInputs[idx].buildRangeInfo.size());
+		for (auto tt = 0; tt < blaseInputs[idx].buildRangeInfo.size(); tt++)
+			maxPrimCount[tt] = blaseInputs[idx].buildRangeInfo[tt].primitiveCount;  // Number of primitives/triangles
+		vkGetAccelerationStructureBuildSizesKHR(
+			_mkDeviceRef.GetDevice(),
+			VK_ACCELERATION_STRUCTURE_BUILD_TYPE_DEVICE_KHR,
+			&buildAsInfo[idx].buildInfo,
+			maxPrimCount.data(),
+			&buildAsInfo[idx].sizeInfo
+		);
+
+		// Extra info
+		asTotalSize += buildAs[idx].sizeInfo.accelerationStructureSize;
+		maxScratchSize = std::max(maxScratchSize, buildAs[idx].sizeInfo.buildScratchSize);
+		nbCompactions += hasFlag(buildAs[idx].buildInfo.flags, VK_BUILD_ACCELERATION_STRUCTURE_ALLOW_COMPACTION_BIT_KHR);
+	}
 }
 
 void MKRaytracer::LoadVkRaytracingExtension()
--- Source/Vulkan/Public/MKRaytracer.h
+++ Source/Vulkan/Public/MKRaytracer.h
@@ -6,23 +6,25 @@
 
 class MKRaytracer
 {
-	// only used as reference from the TLAS
-	struct BLAS
+	struct VkAccelerationStructureKHRInfo 
 	{
-		/* The shape and type of the acceleration structure */
-		VkAccelerationStructureGeometryKHR        geometry;
-		VkAccelerationStructureBuildRangeInfoKHR  buildRangeInfo;
+		VkAccelerationStructureBuildGeometryInfoKHR      buildInfo{ VK_STRUCTURE_TYPE_ACCELERATION_STRUCTURE_BUILD_GEOMETRY_INFO_KHR };
+		VkAccelerationStructureBuildSizesInfoKHR         sizeInfo{ VK_STRUCTURE_TYPE_ACCELERATION_STRUCTURE_BUILD_SIZES_INFO_KHR };
+		const VkAccelerationStructureBuildRangeInfoKHR*  rangeInfo;
+		VkAccelKHR                                       accelStruct; // range
+		VkAccelKHR                                       cleanupAS;
 	};
-
 public:
 	MKRaytracer(MKRaytracer const&) = delete;            // remove copy constructor
 	MKRaytracer& operator=(MKRaytracer const&) = delete; // remove copy assignment operator
 	MKRaytracer(MKDevice& mkDeviceRef, MKGraphicsPipeline& mkPipelineRef);
 	~MKRaytracer();
 
-	void BuildRayTracer();
-	void LoadVkRaytracingExtension();
-	BLAS ObjectToVkGeometryKHR(const OBJModel& model);
+	void   BuildRayTracer();
+	void   LoadVkRaytracingExtension();
+	VkBLAS ObjectToVkGeometryKHR(const OBJModel& model);
+	void   BuildBLAS(const std::vector<VkBLAS>& blases, VkBuildAccelerationStructureFlagsKHR flags = VK_BUILD_ACCELERATION_STRUCTURE_PREFER_FAST_TRACE_BIT_KHR);
+	void   CreateBLAS(const OBJModel& model);
 
 private:
 	/* device reference */
@@ -31,7 +33,7 @@ private:
 	uint32               _graphicsQueueIndex;
 
 	/* BLAS */
-	std::vector<BLAS> _blases;
+	std::vector<VkBLAS> _blases;
 
 #ifdef VK_KHR_acceleration_structure
 	//static PFN_vkGetAccelerationStructureBuildSizesKHR vkGetAccelerationStructureBuildSizesKHR;
