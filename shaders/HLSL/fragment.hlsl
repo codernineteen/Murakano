cfdb1e322a48b497e11f4bee07e1ed5eb52bcf9e
codernineteen <wjsdPcks34@gmail.com>
2024-07-16 19:40:11 +0900

    implemented diffuse shading in HLSL

--- shaders/HLSL/fragment.hlsl
+++ shaders/HLSL/fragment.hlsl
@@ -1,9 +1,22 @@
 // pixel shader input
 struct PSInput
 {
-    float3 Color : COLOR;
-    float2 TexCoord : TEXCOORD;
+    float3 WorldPos    : POSITION0;
+    float3 WorldNormal : NORMAL0;
+    float3 ViewDir     : VIEW0;
+    float2 TexCoord    : TEXCOORD0;
 };
+
+struct PushConstantRaster 
+{
+    float4x4 ModelMat;
+    float3   LightPosition;
+    float    LightIntensity;
+    int      LightType;
+};
+
+[[vk::push_constant]]
+PushConstantRaster pc;
 
 // texture sampler binding
 [[vk::combinedImageSampler]]
@@ -11,10 +24,18 @@ Texture2D texSampler : register(t1);
 [[vk::combinedImageSampler]]
 SamplerState samplerState : register(s1);
 
+static float3 computeDiffuseColor(float4 diffuseColor, float3 viewDir, float3 lightDir, float3 normal)
+{
+    float dotNL = dot(lightDir, normal);
+    float3 outColor = diffuseColor.rgb * saturate(dotNL); // clamp the value between 0 and 1
+    return outColor;
+}
+
 
 float4 main(PSInput input) : SV_Target
 {
 
-    float4 outColor = texSampler.Sample(samplerState, input.TexCoord);
+    float4 texColor = texSampler.Sample(samplerState, input.TexCoord);
+    float4 outColor = float4(computeDiffuseColor(texColor, input.ViewDir, pc.LightPosition, input.WorldNormal), 1.0f);
     return outColor;
 }
--- shaders/HLSL/vertex.hlsl
+++ shaders/HLSL/vertex.hlsl
@@ -1,35 +1,64 @@
+/*
+* ----- Definition ------
+*/
 struct VSInput
 {
     [[vk::location(0)]] float3 Position : POSITION0;
-    [[vk::location(1)]] float3 Color : COLOR0;
+    [[vk::location(1)]] float3 Normal   : NORMAL0;
     [[vk::location(2)]] float2 TexCoord : TEXCOORD0;
+};
+
+struct VSOutput
+{
+    float4 pos : SV_POSITION;
+    [[vk::location(0)]] float3 WorldPos    : POSITION0;
+    [[vk::location(1)]] float3 WorldNormal : NORMAL0;
+    [[vk::location(2)]] float3 ViewDir     : VIEW0;
+    [[vk::location(3)]] float2 TexCoord    : TEXCOORD0;
 };
 
 struct UBO
 {
     float4x4 mvpMat;
+    float4x4 viewInverseMat;
 };
+
+struct PushConstantRaster 
+{
+    float4x4 ModelMat;
+    float3   LightPosition;
+    float    LightIntensity;
+    int      LightType;
+};
+
+[[vk::push_constant]]
+PushConstantRaster pc;
 
 cbuffer ubo : register(b0, space0)
 {
     UBO ubo;
 }
 
-struct VSOutput
-{
-    float4 Pos : SV_POSITION;
-    [[vk::location(0)]] float3 Color : COLOR0;
-    [[vk::location(1)]] float2 TexCoord : TEXCOORD0;
-};
+/*
+* ----- Main ------
+*/
+
 
 VSOutput main(VSInput input, uint VertexIndex : SV_VertexID)
 {
     VSOutput output = (VSOutput) 0;
-    output.Color = input.Color;
     
-    float4 pos = float4(input.Position, 1.0f);
-    output.Pos = mul(pos, ubo.mvpMat);
-    output.TexCoord = input.TexCoord;
+    // vec3 to vec4
+    float4 pos      = float4(input.Position, 1.0f);
+
+    // calculate the view direction
+    float4 cameraOrigin = normalize(mul(float4(4.0f, 0.0f, 0.0f, 1.0f), ubo.viewInverseMat));
+    
+    output.ViewDir     = normalize(input.Position.xyz - cameraOrigin.xyz);
+    output.pos         = mul(pos, ubo.mvpMat); // transform model to clip space
+    output.WorldPos    = input.Position;
+    output.WorldNormal = normalize(mul(input.Normal, (float3x3)pc.ModelMat)); // downcast model matrix to 3x3 matrix first. Then multiply with normal
+    output.TexCoord    = input.TexCoord;
    
     return output;
 }
