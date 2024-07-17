719f8bab804d0f6fa12a6814abbe77a3f314d487
codernineteen <wjsdPcks34@gmail.com>
2024-07-17 19:39:33 +0900

    - modified logic of obj model parsing (per face) - implemented model transformations - support multiple textures and added new field to store a series of unique_ptr

--- Source/Data/Public/OBJModel.h
+++ Source/Data/Public/OBJModel.h
@@ -10,23 +10,49 @@
 struct OBJModel
 {
 public:
-	OBJModel(MKDevice& mkDeviceRef, const std::string modelPath, const std::string texturePath);
+	OBJModel(MKDevice& mkDeviceRef);
 	~OBJModel();
-	void LoadModel();
+	
+	/* load and destroy model */
+	void LoadModel(const std::string& modelPath, const std::vector<TextureMetadata>& textureParams);
+	void DestroyModel();
+
+	/* getter */
+	auto GetModelMatrix() const { 
+#ifdef USE_HLSL
+		auto modelMat = XMMatrixTranspose(modelMatrix);
+		return modelMat;
+#else
+		return modelMatrix;
+#endif
+	}
+
+	/* model transformations */
+	void Scale(float scaleX, float scaleY, float scaleZ);
+	void RotateX(float deg);
+	void RotateY(float deg);
+	void RotateZ(float deg);
 
 private:
-	VkImageAllocated  _vkTextureImage;
-	VkImageView       _vkTextureImageView;
-
-private:
-	const std::string _modelPath;
-	const std::string _texturePath;
+	std::string _modelPath;
 
 public:
-	Texture                                         vikingTexture;
+	/* texture */
+	std::vector<std::unique_ptr<Texture>> textures;
+	
+	/* vertex */
 	std::vector<Vertex>                             vertices;
 	std::unordered_map<Vertex, uint32, VertexHash>  uniqueVertices; // unique vertices with key as vertex and value as index
 	std::vector<uint32>                             indices;
+
+	/* stacked model transformation matrices */
+#ifdef USE_HLSL
+	XMMATRIX modelMatrix;
+#else
+	glm::mat4 modelMatrix;
+#endif
+
+	MKDevice& _mkDeviceRef;
 };
 
 struct OBJInstance
