284ff06c7ccd3da119262aef6ad1c7be6b78b41e
codernineteen <wjsdPcks34@gmail.com>
2024-07-17 19:43:23 +0900

    implemented new api to write multiple images to descriptor in a single call

--- Source/RHI/Vulkan/Public/DescriptorManager.h
+++ Source/RHI/Vulkan/Public/DescriptorManager.h
@@ -26,29 +26,38 @@ public:
 	void                   CreateDescriptorSetLayout(VkDescriptorSetLayout& layout);
 	void                   AllocateDescriptorSet(std::vector<VkDescriptorSet>& descriptorSets, VkDescriptorSetLayout layout);
 	void                   WriteBufferToDescriptorSet(VkBuffer buffer, VkDeviceSize offset, VkDeviceSize range, uint32 dstBinding, VkDescriptorType descriptorType);
-	void                   WriteImageToDescriptorSet(VkImageView imageView, VkSampler imageSampler, VkImageLayout imageLayout, uint32 dstBinding, VkDescriptorType descriptorType);
+	void                   WriteImageToDescriptorSet(VkImageView imageView, VkImageLayout imageLayout, uint32 dstBinding, VkDescriptorType descriptorType);
+	void                   WriteImageArrayToDescriptorSet(VkDescriptorImageInfo* imageInfosPtr, uint32 sizeOfArray, uint32 dstBinding, VkDescriptorType descriptorType);
+	void                   WriteSamplerToDescriptorSet(VkSampler sampler, uint32 dstBinding, VkDescriptorType descriptorType);
 	void                   WriteAccelerationStructureToDescriptorSet(const VkAccelerationStructureKHR* as, uint32 dstBinding);
 	void                   UpdateDescriptorSet(VkDescriptorSet descriptorSet);
 	void                   ResetDescriptorPool();
+
 private:
 	/* create depth buffer resources */
 	bool HasStencilComponent(VkFormat format) { return format == VK_FORMAT_D32_SFLOAT_S8_UINT || format == VK_FORMAT_D24_UNORM_S8_UINT; }
 
 
 private:
-	/* descriptor pool */
-	std::vector<VkDescriptorPoolSize>       _vkDescriptorPoolSizes = {
-		{VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER,         MAX_FRAMES_IN_FLIGHT},
-		{VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, MAX_FRAMES_IN_FLIGHT}
+	/* descriptor pool sizes */
+	std::vector<VkDescriptorPoolSize> _vkDescriptorPoolSizes = {
+		{VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER,MAX_FRAMES_IN_FLIGHT},
+		{VK_DESCRIPTOR_TYPE_SAMPLED_IMAGE, MAX_FRAMES_IN_FLIGHT * 10},
+		{VK_DESCRIPTOR_TYPE_SAMPLER, MAX_FRAMES_IN_FLIGHT * 2}
 	};
-	std::vector<VkDescriptorSetLayoutBinding>                   _vkWaitingBindings = std::vector<VkDescriptorSetLayoutBinding>();
+
+	/* waiting layout bindings */
+	std::vector<VkDescriptorSetLayoutBinding>                   _vkWaitingBindings    = std::vector<VkDescriptorSetLayoutBinding>();
+	
+	/* descriptor info shared pointers */
 	std::unordered_set<std::shared_ptr<VkDescriptorBufferInfo>> _vkWaitingBufferInfos = std::unordered_set<std::shared_ptr<VkDescriptorBufferInfo>>();
-	std::unordered_set<std::shared_ptr<VkDescriptorImageInfo>>  _vkWaitingImageInfos = std::unordered_set<std::shared_ptr<VkDescriptorImageInfo>>();
+	std::unordered_set<std::shared_ptr<VkDescriptorImageInfo>>  _vkWaitingImageInfos  = std::unordered_set<std::shared_ptr<VkDescriptorImageInfo>>();
+	
 	std::unordered_set<std::shared_ptr<VkWriteDescriptorSetAccelerationStructureKHR>> _vkWaitingAsWrites = std::unordered_set<std::shared_ptr<VkWriteDescriptorSetAccelerationStructureKHR>>();
 	std::vector<VkWriteDescriptorSet>                           _vkWaitingWrites = std::vector<VkWriteDescriptorSet>();
 
-	std::vector<VkDescriptorPool>                               _vkDescriptorPoolReady; // available descriptor pool
-	std::vector<VkDescriptorPool>                               _vkDescriptorPoolFull;  // full descriptor pool 
+	std::vector<VkDescriptorPool> _vkDescriptorPoolReady; // available descriptor pool
+	std::vector<VkDescriptorPool> _vkDescriptorPoolFull;  // full descriptor pool 
 
 	/* max set count per pool */
 	uint32 _setsPerPool = 100; // default is 100, max is 4092
