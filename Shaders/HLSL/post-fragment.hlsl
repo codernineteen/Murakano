4f955b68cf57fd699016ef7b6df7746db43c5f14
codernineteen <wjsdPcks34@gmail.com>
2024-08-05 15:10:58 +0900

    resolved an typo in vertex position (z-component value 1.0f -> 0.0f)

--- Shaders/HLSL/post-fragment.hlsl
+++ Shaders/HLSL/post-fragment.hlsl
@@ -1,7 +1,6 @@
 struct PSInput
 {
-    float4 pos : SV_POSITION;
-	[[vk::location(0)]] float2 OutUV : TEXCOORD0;
+	[[vk::location(0)]] float2 UV : TEXCOORD0;
 };
 
 [[vk::combinedImageSampler]]
@@ -12,9 +11,12 @@ SamplerState samplerState : register(s0); // binding point 0
 
 float4 main(PSInput input) : SV_Target
 {
-	float2 uv = input.OutUV;
-	float gamma = 1.0f / 2.2f;
-	float4 outColor = pow(inTexture.Sample(samplerState, uv).rgba, float4(gamma, gamma, gamma, gamma)); // sample color from texture.
+	float2 uv = input.UV;
+	// gamma correction
+	/*float gamma = 1.0f / 2.2f;
+	float4 outColor = pow(inTexture.Sample(samplerState, uv).rgba, float4(gamma, gamma, gamma, gamma)); */
+
+	float4 outColor = inTexture.Sample(samplerState, uv);
 
 	return outColor;
 }
--- Shaders/HLSL/post-vertex.hlsl
+++ Shaders/HLSL/post-vertex.hlsl
@@ -1,7 +1,7 @@
 struct VSOutput
 {
-    float4 pos : SV_POSITION;
-    [[vk::location(0)]] float2 OutUV : TEXCOORD0;
+    float4 Pos : SV_Position;
+    [[vk::location(0)]] float2 UV : TEXCOORD0;
 };
 
 /*
@@ -12,9 +12,9 @@ struct VSOutput
 VSOutput main(uint VertexIndex : SV_VertexID)
 {
     VSOutput output = (VSOutput)0;
-
-    output.OutUV = float2((VertexIndex << 1) & 2, VertexIndex & 2);
-    output.pos = float4(output.OutUV * 2.0f - 1.0f, 1.0f, 1.0f);
+    
+    output.UV = float2((VertexIndex << 1) & 2, VertexIndex & 2);
+    output.Pos = float4(output.UV * 2.0f - 1.0f, 0.0f, 1.0f);
 
     return output;
 }
