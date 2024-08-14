bf0399cb4e44e159505f285cfdb21d5bb4602de9
codernineteen <wjsdPcks34@gmail.com>
2024-08-14 19:54:59 +0900

    implemented microfacet specular BRDF (GGX NDF)

--- shaders/HLSL/fragment.hlsl
+++ shaders/HLSL/fragment.hlsl
@@ -54,6 +54,128 @@ Texture2DArray textures : register(t0); // binding array to register t0
 [[vk::binding(0, 1)]]                     // sampler descriptor binding
 SamplerState samplerState : register(s0); // binding sampler to register s0
 
+// ------------------- Constant -------------------------
+static const float PI = 3.14159265f;
+
+// -------------------- PBR ----------------------------- 
+
+static float ginnekenChiClamp(float angle)
+{
+    return saturate(4.41 * angle / (4.41 * angle + 1)); // clamp Chi function result between 0 and 1
+}
+
+// a_variable for parameter in lambda functions
+static float avar(float3 n, float3 s, float roughness)
+{
+    float dotNS = dot(n, s);
+
+    return dotNS / (roughness * sqrt(1 - pow(dotNS, 2)));
+}
+
+static float avarSqaure(float3 n, float s, float roughness)
+{
+    float dotNS = dot(n, s);
+
+    return dotNS * dotNS / (roughness * roughness * (1 - pow(dotNS, 2)));
+}
+
+/*
+    Lambda functions
+*/
+static float lambdaApproxBeckmann(float a)
+{
+    if (a >= 1.6)
+    {
+        return 0;
+    }
+    else
+    {
+        float poweredA = pow(a, 2);
+        return (1 - 1.259 * a + 0.396 * poweredA) / (3.535 * a + 2.181 * poweredA);
+    }
+}
+
+static float lambdaGGX(float aSquare)
+{
+    return (-1 + sqrt(1 + 1 / aSquare)) / 2;
+}
+
+/*
+    Smith Shadowing Masking Functions
+*/
+
+static float heightCorrelatedSmithG2(float3 l, float3 v, float3 m, float roughness)
+{
+    float chiMV = ginnekenChiClamp(dot(m, v));
+    float chiML = ginnekenChiClamp(dot(m, l));
+    float aFromV = avar(m, v, roughness);
+    float aFromL = avar(m, l, roughness);
+    float lambdaV = lambdaApproxBeckmann(aFromV);
+    float lambdaL = lambdaApproxBeckmann(aFromL);
+
+    return chiMV * chiML / (1 + lambdaV + lambdaL);
+}
+
+static float heightCorrelatedSmithG2GGX(float3 n, float3 l, float3 v, float alpha)
+{
+    float alphaSquare = alpha * alpha;
+
+    float muIn = saturate(dot(n, l));
+    float muOut = saturate(dot(n, v));
+
+    float denominator = muOut * sqrt(alphaSquare + muIn * (muIn - alphaSquare * muIn)) + muIn * sqrt(alphaSquare + muOut * (muOut - alphaSquare * muOut));
+
+    return 0.5 / denominator;
+}
+
+/*
+    Normal Distribution Functions
+*/
+
+static float beckmannNDF(float dotNM, float roughness)
+{
+    float dotNMSquare = dotNM * dotNM;
+    float roughnessSquare = roughness * roughness;
+    float chiDotNM = ginnekenChiClamp(dotNM);
+
+    // For all microfacet normal that point to under macrosurface, clamp to zero.
+    return (chiDotNM / (PI * roughnessSquare * dotNMSquare * dotNMSquare)) * exp((dotNMSquare - 1) / (roughnessSquare * dotNMSquare));
+}
+
+/*
+ GGX-NDF
+    ag is a parameter that is equal to r^2, where r is user-selective roughness between 0 and 1
+*/
+static float GGXNDF(float dotNM, float ag)
+{
+    float agSquare = ag * ag;
+    return ginnekenChiClamp(dotNM) * agSquare / PI * pow((1 + (dotNM * dotNM) * (agSquare - 1)), 2);
+}
+
+// use this instead of original form of Fresnel equation
+static float schlickApprox(float ior, float3 n, float3 l)
+{
+    float fZero = (ior - 1) * (ior - 1) / (ior + 1) * (ior + 1);
+    float dotNV = saturate(dot(n, l)); // clmap 0 ~ 1
+
+    // interpolation
+    return fZero + (1 - fZero) * (1 - pow(dotNV, 5));
+}
+
+static float microSpecularBRDF(float3 l, float3 v, float3 n, float roughness)
+{
+    // compute half vector
+    float3 h = normalize(l + v);
+
+    // NDF : GGX
+    float ior = 1.4; // skin in air
+    float F  = schlickApprox(ior, h, l);
+    float G2 = heightCorrelatedSmithG2GGX(h, l, v, roughness);
+    float D = GGXNDF(dot(n, h), roughness * roughness);
+
+    return F * G2 * D;
+}
+
 // ------------------ HELPER FUNCTIONS ------------------
 static float3 computeDiffuseColor(float4 diffuseColor, float3 viewDir, float3 lightDir, float3 normal)
 {
@@ -69,14 +191,10 @@ static float3 computeSpecularColor(float4 specularColor, float3 viewDir, float3 
     float3 V = normalize(-viewDir);
     float3 R = reflect(-lightDir, normal);
     float dotRV = saturate(dot(V, R)); // clampe the value between 0 and 1
-    float specularTerm = pow(dotRV, shininess);
+    //float specularTerm = pow(dotRV, shininess);
+    float specularTerm = microSpecularBRDF(lightDir, viewDir, normal, 1 / shininess);
 
     return specularColor.rgb * specularTerm;
-}
-
-static float3 createTestColorBasedNormal(float3 normal)
-{
-    return dot(float3(1.0f, 1.0f, 1.0f), normal);
 }
 
 
