eb5ad5e57d4a55031ea2d6532dde28ed94190bd1
codernineteen <wjsdPcks34@gmail.com>
2024-08-24 16:16:35 +0900

    modify binding register.

--- Shaders/HLSL/post-fragment.hlsl
+++ Shaders/HLSL/post-fragment.hlsl
@@ -4,17 +4,17 @@ struct PSInput
 };
 
 [[vk::combinedImageSampler]]
-Texture2D inTexture : register(t0); // bindding point 0
+Texture2D inTexture : register(t3); // bindding point 0
 
 [[vk::combinedImageSampler]]
-SamplerState samplerState : register(s0); // binding point 0
+SamplerState samplerState : register(s3); // binding point 0
 
 float4 main(PSInput input) : SV_Target
 {
 	float2 uv = input.UV;
 	// gamma correction
-	/*float gamma = 1.0f / 2.2f;
-	float4 outColor = pow(inTexture.Sample(samplerState, uv).rgba, float4(gamma, gamma, gamma, gamma)); */
+	//float gamma = 1.0f / 2.2f;
+	//float4 outColor = pow(inTexture.Sample(samplerState, uv).rgba, float4(gamma, gamma, gamma, gamma)); 
 
 	float4 outColor = inTexture.Sample(samplerState, uv);
 
